5f59f71f96318ec32cb5ccb10aea07d173e7bf8c
pywzzz <1021640832@qq.com>
2023-04-07 15:05:07 +0800

    83： 1.弄了个Observe组件，在主页的底部加了些静态，折线图、表格、饼图啥的。

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -2,17 +2,20 @@
 	<div>
 		<Card></Card>
 		<Sale></Sale>
+		<Observe></Observe>
 	</div>
 </template>
 
 <script>
 import Card from "./Card";
 import Sale from "./Sale";
+import Observe from "./Observe";
 export default {
 	name: "",
 	components: {
 		Card,
 		Sale,
+		Observe,
 	},
 };
 </script>
