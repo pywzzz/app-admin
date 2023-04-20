b96e42334a88107828193d9752f4f087185151fb
pywzzz <1021640832@qq.com>
2023-04-20 16:27:24 +0800

    1.在SkuForm组件中也实现了：默认图片不可被勾选、“设置默认”按钮只有在被勾选时才可被点击。

--- src/views/product/Spu/SkuForm/index.vue
+++ src/views/product/Spu/SkuForm/index.vue
@@ -60,6 +60,7 @@
 					border
 					:data="spuImageList"
 					@selection-change="handleSelectionChange"
+					ref="imageTable"
 				>
 					<el-table-column
 						type="selection"
@@ -80,6 +81,7 @@
 						<template slot-scope="{ row }">
 							<el-button
 								type="primary"
+								:disabled="!row.isSelected"
 								v-if="row.isDefault == 0"
 								@click="changeDefault(row)"
 								>设置默认</el-button
@@ -145,10 +147,12 @@ export default {
 			// 获取图片的数据
 			let result0 = await this.$API.spu.reqSpuImageLIst(spu.id);
 			if (result0.code == 200) {
-				// 收集数据的同时为每个图片数据加个叫isDefault的属性，用来今后确定这是否是默认图片
 				let list = result0.data;
 				list.forEach((item) => {
+					// 收集数据的同时为每个图片数据加个叫isDefault的属性，用来今后确定这是否是默认图片
 					item.isDefault = 0;
+					// 收集数据的同时为每个图片数据加个叫isSelected的属性，用于动态控制“设为默认”按钮是否可被点击
+					item.isSelected = false;
 				});
 				this.spuImageList = list;
 			}
@@ -169,6 +173,19 @@ export default {
 		},
 		// 这是那个复选框的回调，这个params是勾中的那一堆东西的数据
 		handleSelectionChange(params) {
+			this.spuImageList.forEach((spuImage) => {
+				// 如果includes(spuImage)，那就意味着被勾选了，则返回true，刚好赋给isSelected
+				spuImage.isSelected = params.includes(spuImage);
+
+				// 如果默认图片被取消勾选，撤销操作并显示警告
+				const isSelected = params.includes(spuImage);
+				if (spuImage.isDefault === 1 && !isSelected) {
+					// 重新勾上
+					this.$refs.imageTable.toggleRowSelection(spuImage, true);
+					this.$message.warning("默认图片不能被取消勾选");
+					return;
+				}
+			});
 			this.imageList = params;
 		},
 		changeDefault(row) {
